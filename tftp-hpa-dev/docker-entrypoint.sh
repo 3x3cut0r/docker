883bc29ed0cc08295839cfaab7922e4910c530a2
3x3cut0r <executor55@gmx.de>
2021-01-28 21:00:45 +0100

    tftp-hpa-dev/docker-entrypoint.sh

--- tftp-hpa-dev/docker-entrypoint.sh
+++ tftp-hpa-dev/docker-entrypoint.sh
@@ -1,45 +1,31 @@
 #!/bin/sh
-# create and set permissions to TFTPROOT
-mkdir -p "${TFTPROOT}"
-chown -R "${UID}":"${GID}" "${TFTPROOT}"
-chmod -R 0777 "${TFTPROOT}"
-# set permissions for MAPFILE
-chown "${UID}":"${GID}" "${MAPFILE}"
-chmod 0400 "${MAPFILE}"
 set -e
 # if started without args, exec in.tftpd
 if [ "$#" = "0" ]; then
     param=""
-    if [ "$ADDRESS" != "" ]; then param="${param} --address $ADDRESS"; fi
     if [ "$BLOCKSIZE" != "" ]; then param="${param} --blocksize $BLOCKSIZE"; fi
     if [ "$CREATE" = "1" ]; then param="${param} --create"; fi
-    if [ "$FOREGROUND" = "1" ]; then param="${param} --foreground"; fi
-    if [ "$IPV4" = "1" ]; then param="${param} --ipv4"; fi
-    if [ "$IPV6" = "1" ]; then param="${param} --ipv6"; fi
-    if [ "$LISTEN" = "1" ]; then param="${param} --listen"; fi
     if [ "$MAPFILE" != "" ]; then param="${param} --map-file $MAPFILE"; fi
     if [ "$PERMISSIVE" = "1" ]; then param="${param} --permissive"; fi
-    if [ "$PIDFILE" != "" ]; then param="${param} --pidfile" $PIDFILE; fi
     if [ "$PORTRANGE" != "" ]; then param="${param} --port-range $PORTRANGE"; fi
     if [ "$REFUSE" != "" ]; then param="${param} --refuse $REFUSE"; fi
     if [ "$RETRANSMIT" != "" ]; then param="${param} --retransmit $RETRANSMIT"; fi
     if [ "$SECURE" = "1" ]; then param="${param} --secure"; fi
     if [ "$TIMEOUT" != "" ]; then param="${param} --timeout $TIMEOUT"; fi
     if [ "$UMASK" != "" ]; then param="${param} --umask $UMASK"; fi
-    if [ "$USER" != "" ]; then param="${param} --user $USER"; fi
     if [ "$VERBOSE" = "1" ]; then param="${param} --verbose"; fi
     if [ "$VERBOSITY" = "1" ]; then param="${param} --verbosity $VERBOSITY"; fi
-    param="${param} ${TFTPROOT}"
-    exec gosu "${UID}":"${GID}" in.tftpd ${param} 2>&1
+    param="--foreground --address 0.0.0.0:9069 --user tftp ${param} /tftpboot"
+    echo "INFO: in.tftpd ${param}"
+    exec in.tftpd ${param}
 else
-    # if the first arg is "in.tftpd" ...
-    if [ "$1" = "in.tftpd" ]; then
-        exec gosu "${UID}":"${GID}" $@ 2>&1
     # if first arg looks like a flag, assume we want to run in.tftpd
-    elif [ "$( echo "$1" | cut -c1 )" = "-" ]; then
-        exec gosu "${UID}":"${GID}" in.tftpd $@ 2>&1
+    if [ "$( echo "$1" | cut -c1 )" = "-" ]; then
+        echo "INFO: in.tftpd $@"
+        exec in.tftpd $@
     # if first arg is either 'in.tftpd' or args of 'in.tftpd'
     else
-        exec gosu "${UID}":"${GID}" $@ 2>&1
+        echo "INFO: $@"
+        exec $@
     fi
 fi
