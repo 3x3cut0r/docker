d1a4842177e7a5243cfbd58be414672cef961acb
3x3cut0r <executor55@gmx.de>
2021-01-29 09:51:13 +0100

    tftp-hpa-dev/docker-entrypoint.sh

--- tftp-hpa-dev/docker-entrypoint.sh
+++ tftp-hpa-dev/docker-entrypoint.sh
@@ -5,25 +5,28 @@ if [ "$#" = "0" ]; then
     param=""
     if [ "$BLOCKSIZE" != "" ]; then param="${param} --blocksize $BLOCKSIZE"; fi
     if [ "$CREATE" = "1" ]; then param="${param} --create"; fi
+    if [ "$PORTRANGE" != "" ]; then param="${param} --port-range $PORTRANGE"; fi
     if [ "$MAPFILE" != "" ]; then param="${param} --map-file $MAPFILE"; fi
     if [ "$PERMISSIVE" = "1" ]; then param="${param} --permissive"; fi
-    if [ "$PORTRANGE" != "" ]; then param="${param} --port-range $PORTRANGE"; fi
     if [ "$REFUSE" != "" ]; then param="${param} --refuse $REFUSE"; fi
     if [ "$RETRANSMIT" != "" ]; then param="${param} --retransmit $RETRANSMIT"; fi
     if [ "$SECURE" = "1" ]; then param="${param} --secure"; fi
     if [ "$TIMEOUT" != "" ]; then param="${param} --timeout $TIMEOUT"; fi
     if [ "$UMASK" != "" ]; then param="${param} --umask $UMASK"; fi
     if [ "$VERBOSE" = "1" ]; then param="${param} --verbose"; fi
-    if [ "$VERBOSITY" = "1" ]; then param="${param} --verbosity $VERBOSITY"; fi
+    if [ "$VERBOSITY" != "" ]; then param="${param} --verbosity $VERBOSITY"; fi
     param="--foreground --address 0.0.0.0:9069 --user tftp ${param} /tftpboot"
     echo "INFO: in.tftpd ${param}"
     exec in.tftpd ${param}
 else
     # if first arg looks like a flag, assume we want to run in.tftpd
     if [ "$( echo "$1" | cut -c1 )" = "-" ]; then
-        echo "INFO: in.tftpd $@"
-        exec in.tftpd $@
-    # if first arg is either 'in.tftpd' or args of 'in.tftpd'
+        echo "INFO: in.tftpd --foreground --address 0.0.0.0:9069 --user tftp $@"
+        exec in.tftpd --foreground --address 0.0.0.0:9069 --user tftp $@
+    # if the first arg is "in.tftpd" ...
+    elif [ "$1" = "in.tftpd" ]; then
+        exec in.tftpd --foreground --address 0.0.0.0:9069 --user tftp "${@:9}"
+    # if first arg doesn't looks like a flag
     else
         echo "INFO: $@"
         exec $@
