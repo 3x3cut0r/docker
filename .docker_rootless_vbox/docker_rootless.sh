29c4acfa40d3d3db84c0dee4db8cb8a1123cd102
3x3cut0r <executor55@gmx.de>
2021-02-19 22:53:08 +0100

    docker_rootless.sh

--- .docker_rootless_vbox/docker_rootless.sh
+++ .docker_rootless_vbox/docker_rootless.sh
@@ -55,7 +55,7 @@
 # run as root first without args
 if [ "$#" = "0" ]; then
     # check root
-    if [ $UID -ne 0 ]; then echo "run as root (uid=0) to install prerequisites"; exit 1; fi
+    if [ $UID -ne 0 ]; then echo -e "run as root (uid=0) to install prerequisites\nif you want do install docker, do:\n./docker_rootless.sh install"; exit 1; fi
 
     # check first run
     printf '\n\e[0;33m%-6s\e[m\n' " ==> APT: install prerequisites ... \n"
