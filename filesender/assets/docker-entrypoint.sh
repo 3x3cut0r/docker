54904405e756cfe91a013968dd6febc348d84f5b
3x3cut0r <executor55@gmx.de>
2022-04-24 15:47:21 +0200

    filesender/assets/docker-entrypoint.sh

--- filesender/assets/docker-entrypoint.sh
+++ filesender/assets/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # change file permissions
-chown -R www-data:www-data /opt/* /config /data
+chown -R www-data:www-data /opt /config /data
 
 # exec /usr/local/sbin/runsvdir-init
 exec /usr/local/sbin/runsvdir-init
