a567c958106031fa34d90eb94d0a0bec64a872cc
3x3cut0r <executor55@gmx.de>
2022-04-27 09:38:45 +0200

    filesender/assets

--- filesender/assets/scripts/02-set-permissions.sh
+++ filesender/assets/scripts/02-set-permissions.sh
@@ -9,14 +9,29 @@ FILESENDER_CONFIG_DIR="/config/filesender"
 
 
 # change permissions and ownership of filesender to www-data
-chown -R www-data:www-data  /opt/* \
-                            /config/* \
-                            /data/*
+chown -R root:www-data      /config/filesender
+
+chown -R www-data:www-data  /data/* \
+                            /config/filesender/www
+
+
+# chown -R root:root        /config/fpm/* \
+#                           /config/simplesamlphp/*
 
 chmod -R 755    /opt/* \
                 /config/* \
                 /data/* \
-                /usr/local/sbin/* \
-                /etc/service/*
+                /usr/local/sbin/*
 
-chmod -R o-rwx  /data/*
+chmod 775       /etc/service/*/run \
+                /etc/nginx/sites-*
+
+chmod 664       /etc/nginx/fastcgi_params \
+                /etc/nginx/sites-available/default
+
+chmod -R 644    /config/fpm \
+                /config/simplesamlphp
+
+
+chmod -R o-rwx  /data/* \
+                /config/filesender/config/*
