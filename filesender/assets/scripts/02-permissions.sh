a2353a8c483594862a768afc77466844574f6c32
3x3cut0r <executor55@gmx.de>
2022-05-13 15:26:11 +0200

    filesender/assets

--- filesender/assets/scripts/02-permissions.sh
+++ filesender/assets/scripts/02-permissions.sh
@@ -11,7 +11,7 @@ FILESENDER_CONFIG_DIR="/config/filesender"
 # change permissions and ownership of filesender to www-data
 chown -R www-data:www-data  /opt/*
 
-chown -R www-data:root      /data/* \
+chown -R root:www-data      /data/* \
                             /config/filesender/config \
                             /config/simplesamlphp/config
 
