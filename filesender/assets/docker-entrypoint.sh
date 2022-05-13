17a01e0457be0e1493b6e299f96008ea3dcd0eff
3x3cut0r <executor55@gmx.de>
2022-05-13 15:18:39 +0200

    filesender/assets/docker-entrypoint.sh

--- filesender/assets/docker-entrypoint.sh
+++ filesender/assets/docker-entrypoint.sh
@@ -8,6 +8,7 @@ for script in $(ls | grep '.sh$' ); do
     echo "exec: /scripts/$script"
     /bin/bash ./"$script"
 done
+echo ""
 
 # exec /usr/local/sbin/runsvdir-init
 exec /usr/local/sbin/runsvdir-init
