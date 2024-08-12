edad7f7b8073058f3c2cc277d40709aebd282194
3x3cut0r <executor55@gmx.de>
2024-08-12 11:09:52 +0200

    VERSION added

--- whisper-gui/docker-entrypoint.sh
+++ whisper-gui/docker-entrypoint.sh
@@ -6,6 +6,9 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # set language
 sed -i 's/"language": "[^"]*"/"language": "'"${LANGUAGE:-"en"}"'"/' /whisper-gui/configs/config.json
 
+# print privategpt version
+echo "whisper-gui version: $(cat /VERSION)"
+
 # run app
 source /opt/conda/etc/profile.d/conda.sh && \
 conda activate whisper-gui && \
