cb06012f3b895fdf9ae85e0008825c583f7fdb4e
3x3cut0r <executor55@gmx.de>
2024-08-12 07:50:07 +0200

    LSNGUAGE support added

--- whisper-gui/docker-entrypoint.sh
+++ whisper-gui/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# set timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# set language
+sed -i 's/"language": "[^"]*"/"language": "'"${LANGUAGE:-"en"}"'"/' /whisper-gui/configs/config.json
+
 # run app
 source /opt/conda/etc/profile.d/conda.sh && \
 conda activate whisper-gui && \
